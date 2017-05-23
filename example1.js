77d48f5571736c1224ab75fd60a8e411ada19065
Nick Kasten <kastencode@gmail.com>
2017-05-23 12:50:02 -0500

    adding interactive prompt. maintaining state

--- example1.js
+++ example1.js
@@ -3,6 +3,7 @@
 
 require('dotenv').config()
 var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+var prompt = require('prompt-sync')();
 
 // Set up Conversation service wrapper.
 var conversation = new ConversationV1({
@@ -21,9 +22,22 @@ function processResponse(err, response) {
     console.error(err); // something went wrong
     return;
   }
-  
+
+  // If an Intent was detected, log it to the console.
+  if (response.intents.length > 0) {
+    console.log('Detected intent: #' + response.intents[0].intent);
+  }
+
   // Display the output from dialog, if any.
   if (response.output.text.length != 0) {
       console.log(response.output.text[0]);
   }
+
+  // Prompt the user for the next round of input.
+  var newMessageFromUser = prompt('>> ');
+  // Send back the context to maintain state
+  conversation.message({
+    input: { text: newMessageFromUser },
+    context: response.context
+  }, processResponse)
 }
